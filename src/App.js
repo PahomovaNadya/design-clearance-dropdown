8d0ac8c3cae07585b4a64d578aa7d82ee919a52d
unknown <nvn7ya@yahoo.com>
2018-06-13 23:36:31 +0300

    final version, subject to corrections

--- src/App.js
+++ src/App.js
@@ -1,214 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UISelect from './ui.select.jsx';
-import FreeTextSearch from './freeTextSearch.jsx';
+import UISelect from './ui.select.jsx'
 
 import data1 from './data/select1.json'
 import data2 from './data/select1.json'
 
-let newName: string="";
-let newID: number;
-
 class App extends Component {
-  constructor(props: DataProps) {
-    super();
-    this.state = {
-      txtOptiondata1: data1[0].name,
-      txtOptiondata2: data2[0].name,
-      txtOptiondata3: data1[0].name,
-      flagCls: [false,false,false],
-      flagFocus: [false,false,false],
-      selectElem: [null,null,null],
-      dataFocus: []
-    };
-    this.keyPressed = this.keyPressed.bind(this);
-    this.FuncFreeTextSearch = this.FuncFreeTextSearch.bind(this);
-    this.OpenCloseDropdown = this.OpenCloseDropdown.bind(this);
-    this.OptionSelected = this.OptionSelected.bind(this);
-    this.SelectListRelArrow = this.SelectListRelArrow.bind(this);
-    this.SaveDate = this.SaveDate.bind(this);
-  }
-  
-  /* checks which key is typed on the keyboard */
-  keyPressed(e)
-  {
-    var keyCode;
-    
-    if (window.event)
-    {
-      keyCode = e.keyCode;
-    }
-    else if (e)
-    {
-      keyCode = e.which;
-    }
-
-    let numAct: number;
-    let txtOptDT1 = this.state.txtOptiondata1;
-    let txtOptDT2 = this.state.txtOptiondata2;
-    let txtOptDT3 = this.state.txtOptiondata3;
-    let idOptDT = this.state.selectElem;
-
-    for(let i=0; i<this.state.flagFocus.length; i++) {
-      if( this.state.flagFocus[i] === true ){
-        numAct = i
-      }
-    }
-    if(numAct !== undefined) {
-      if(keyCode===38) {
-/*===== reaction to pressing arrow UP =====*/
-        this.SelectListRelArrow(this.state.selectElem[numAct],'-1');
-      } else if(keyCode===40) {
-/*===== reaction to pressing arrow DOWN =====*/
-    this.SelectListRelArrow(this.state.selectElem[numAct],'1');
-      } else if(keyCode===13) {
-/* reaction to pressing ENTER. When double ckick does not work, the focus is lost. */
-        this.SelectListRelArrow(this.state.selectElem[numAct],'0');
-        this.OptionSelected (this.state.selectElem[numAct], newName, numAct, this.state.dataFocus);
-      }
-      switch(numAct) {
-        case 0: {
-          txtOptDT1 = newName;
-          idOptDT[0] = newID;
-          break;
-        }
-        case 1: {
-          txtOptDT2 = newName;
-          idOptDT[1] = newID;
-          break;
-        }
-        case 2: {
-          txtOptDT3 = newName;
-          idOptDT[2] = newID;
-          break;
-        }
-      }
-      this.SaveDate(txtOptDT1, txtOptDT2, txtOptDT3, this.state.flagFocus, this.state.flagCls, idOptDT, this.state.dataFocus);
-    }
-  }
-
-  /* printing it’s name and id to console */
-  OptionSelected (evId:number, evName:string, indDrD: number, dataReturned:DataProps){
-    console.log("id:    "+evId+"\nname:  "+evName);
-    let txtOptDT1 = this.state.txtOptiondata1;
-    let txtOptDT2 = this.state.txtOptiondata2;
-    let txtOptDT3 = this.state.txtOptiondata3;
-    let flagFocDr = [false,false,false];
-    flagFocDr[indDrD-1] = true;
-    let flagFocus = [false,false,false];
-    flagFocus[indDrD-1] = true;
-    switch(indDrD){
-      case 1: {
-        txtOptDT1 = evName;
-        break;
-      }
-      case 2: {
-        txtOptDT2 = evName;
-        break;
-      }
-      case 3: {
-        txtOptDT3 = evName;
-        break;
-      }
-    }
-    let flagSelElem = [];
-    for(let i=0; i<this.state.selectElem.length; i++) {
-      if( i===(indDrD-1) ) {
-        flagSelElem[i] = evId;
-      }
-      else{
-        flagSelElem[i] = this.state.selectElem[i];
-      }
-    }
-    this.SaveDate(txtOptDT1, txtOptDT2, txtOptDT3, flagFocus, flagFocDr, flagSelElem, dataReturned);
-  }
-
-  /* It does not work on the ENTER, because the search goes on several JSON and ID may not coincide */
-  FuncFreeTextSearch(txtSearch: string){
-    let txtSearch1: string = this.state.txtOptiondata1;
-    let txtSearch2: string = this.state.txtOptiondata2;
-    let txtSearch3: string = this.state.txtOptiondata3;
-    if (txtSearch) {
-      for(var i=0; i<data1.length; i++) {
-        if( data1[i].name.toLowerCase().includes(txtSearch.toLowerCase()) ){
-          //if(txtSearch1===data1[0].name) {
-            txtSearch1 = data1[i].name;
-          //}
-          //if(txtSearch3===data1[0].name) {
-            txtSearch3 = data1[i].name;
-          //}
-        }
-      }
-      for(var i=0; i<data2.length; i++) {
-        if( data2[i].name.toLowerCase().includes(txtSearch.toLowerCase()) ){
-          //if(txtSearch2===data2[0].name) {
-            txtSearch2 = data2[i].name;
-          //}
-        }
-      }
-    }
-    this.SaveDate(txtSearch1, txtSearch2, txtSearch3, this.state.flagCls, this.state.selectElem, this.state.dataFocus);
-
-  }
-
-  /* determines which of the dropdown in focus */
-  OpenCloseDropdown(flg:boolean, indDr:number, data:any) {
-    let flagFocDr = [];
-    let flagFocus = [];
-    let txtSearch1: string = this.state.txtOptiondata1;
-    let txtSearch2: string = this.state.txtOptiondata2;
-    let txtSearch3: string = this.state.txtOptiondata3;
-    
-    for(let i=0; i<this.state.flagCls.length; i++) {
-      if( i===(indDr-1) ) {
-        flagFocDr[i] = flg;
-        flagFocus[i] = true;
-      }
-      else {
-        flagFocDr[i] = false;
-        flagFocus[i] = false;
-      }
-    }
-    this.SaveDate(txtSearch1, txtSearch2, txtSearch3, flagFocus, flagFocDr, this.state.selectElem, data);
-  }
-
-  /* from the list, relative to the arrows UP and DOWN */
-  SelectListRelArrow(idDataSel: number, chOpe: string) {
-    let iDop: number
-    for(let i=0; i<this.state.dataFocus.length; i++) {
-      if( this.state.dataFocus[i].id === idDataSel ){
-        iDop = i
-      }
-    }
-    if(iDop+(+chOpe)<0) {
-      newName = this.state.dataFocus[this.state.dataFocus.length-1].name;
-      newID = this.state.dataFocus[this.state.dataFocus.length-1].id;
-    } 
-    else if(iDop+(+chOpe)>this.state.dataFocus.length-1) {
-      newName = this.state.dataFocus[0].name;
-      newID = this.state.dataFocus[0].id;
-    }
-    else {
-      newName = this.state.dataFocus[iDop+(+chOpe)].name;
-      newID = this.state.dataFocus[iDop+(+chOpe)].id;
-    }
-  }
-
-  SaveDate(newTxtOpt1: string, newTxtOpt2: string, newTxtOpt3: string, newFlagFocus: Array, newFlagFocDr: Array, newFlagSelElem: Array, newDataReturned: DataProps) {
-    this.setState({
-      txtOptiondata1: newTxtOpt1, 
-      txtOptiondata2: newTxtOpt2,
-      txtOptiondata3: newTxtOpt3,  
-      flagFocus: newFlagFocus,        
-      flagCls: newFlagFocDr,
-      selectElem: newFlagSelElem,
-      dataFocus: newDataReturned
-    });
-  }
-  
   render() {
-    document.onkeydown = this.keyPressed;
     return (
       <div className="App">
         <header className="App-header">
@@ -217,50 +16,26 @@ class App extends Component {
         </header>
         <div className="test-app">
           <div className="row">
-            <FreeTextSearch
-              getSelctedJData={this.FuncFreeTextSearch} 
-              tabIndex={4} 
-            />
-          </div>
-          <div className="col col3">
-            <div className="row">
-{/* When defining the class: col1, col2, col3, 
-* send the parameter widthObj-width to the Component UISelect, 
-* defined in the class: 100, 150, 200 respectively */}
-              <UISelect 
-                options={data1} 
-                firstoption={this.state.txtOptiondata1} 
-                widthObj={200} 
-                flagOpen={this.state.flagCls[0]}  
-                getOpenCloseDropdown={this.OpenCloseDropdown}
-                getOptionSelected={this.OptionSelected}
-                tabIndex={1} 
-              />
+            <div className="col col1">
+              <UISelect options={data1}/>
             </div>
           </div>
+
           <div className="row">
-            <UISelect 
-              options={data2} 
-              firstoption={this.state.txtOptiondata2} 
-              flagOpen={this.state.flagCls[1]} 
-              getOpenCloseDropdown={this.OpenCloseDropdown}
-              getOptionSelected={this.OptionSelected}
-              tabIndex={2} 
-            />
+            <div className="col col2">
+            <UISelect options={data2}/>
+            </div>
           </div>
+
           <div className="row">
-            <UISelect 
-              options={data1} 
-              firstoption={this.state.txtOptiondata3} 
-              flagOpen={this.state.flagCls[2]}
-              getOpenCloseDropdown={this.OpenCloseDropdown}
-              getOptionSelected={this.OptionSelected}
-              tabIndex={3} 
-            />
+            <div className="col col3">
+              <UISelect options={data1}/>
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
+
 export default App;
--- src/ui.select.jsx
+++ src/ui.select.jsx
@@ -2,67 +2,245 @@ import React, { Component } from 'react';
 import LISelect from './li.select.jsx';
 import './App.css';
 
-interface UIselectProps {
-  options: any;
-  widthObj: number;
-  tabIndex: number;
-  firstoption: string;
-  flagOpen: boolean;
-  getOptionSelected: Function;
-  getOpenCloseDropdown: Function;
-}
+let flagFocus = false;
 
-interface UIselectState {
-  flagArrow: boolean;
-  optionSel: string;
-}
-let flagFocus=false;
-
-class UISelect extends Component<UIselectProps, UIselectState> {
-  constructor(props: UIselectProps) {
+class UISelect extends Component {
+  constructor(props) {
     super(props);
-    this.objectFocusFunc = this.objectFocusFunc.bind(this);
+    this.state = {
+      txtOptionid: this.props.options[0].id,
+      txtOptionname: this.props.options[0].name,
+      keyText: "",
+      tabIndex: -1,
+      widthObj: 100,
+      indLi: 0
+    };
+    this.keyPressed = this.keyPressed.bind(this);
+    this.inputFocusFunc = this.inputFocusFunc.bind(this);
+    this.objectBlurFunc = this.objectBlurFunc.bind(this);
+    this.objectOpenCloseFunc = this.objectOpenCloseFunc.bind(this);
+    this.bustList = this.bustList.bind(this);
+    this.selectLineData = this.selectLineData.bind(this);
+    this.printData = this.printData.bind(this);
     this.OptionSelectedFn = this.OptionSelectedFn.bind(this);
   }
-
-  objectFocusFunc(){
-    flagFocus = !flagFocus;
-    setTimeout(() => {
-      this.props.getOpenCloseDropdown(!this.props.flagOpen, this.props.tabIndex, this.props.options);
-    }, 200);
-  }
   
-  /* printing it’s name and id to console */
-  OptionSelectedFn(idLI:number, nameLI:string) {
-    this.props.getOptionSelected(idLI, nameLI, this.props.tabIndex, this.props.options);
-  }
+  keyPressed(e) {
+    var keyCode;
+    
+    if (window.event)
+    {
+      keyCode = e.keyCode;
+    }
+    else if (e)
+    {
+      keyCode = e.which;
+    }
 
+    if(keyCode===9) {
+      /*===== reaction to pressing TAB =====*/
+    } else if(keyCode===38) {
+      /*===== reaction to pressing arrow UP =====*/
+      let numI = this.state.indLi;
+      let objUL = document.getElementsByTagName("ul");
+      let obj = objUL[this.state.tabIndex].getElementsByTagName("li")
+      if( numI === 0 ) {
+        numI = this.props.options.length-1;
+      } else {
+        numI--;
+      }
+      for(let i = 0; i < this.props.options.length; i++){
+        if( i === numI ) {
+          obj[numI].className = "actionSelect";
+          obj[this.state.tabIndex].parentElement.parentElement.scrollTop = numI*20;
+        } else {
+          obj[i].className = "";
+        }
+      }
+      this.printData(this.props.options[numI].id,this.props.options[numI].name);
+      this.setState({
+        indLi: numI,
+        txtOptionid: this.props.options[numI].id,
+        txtOptionname: this.props.options[numI].name
+      });
+    } else if(keyCode===40) {
+      /*===== reaction to pressing arrow DOWN =====*/
+      let numI = this.state.indLi;
+      let objUL = document.getElementsByTagName("ul");
+      let obj = objUL[this.state.tabIndex].getElementsByTagName("li")
+      if( numI === this.props.options.length-1 ) {
+        numI = 0;
+      } else {
+        numI++;
+      }
+      for(let i = 0; i < this.props.options.length; i++){
+        if( i === numI ) {
+          obj[numI].className = "actionSelect";
+          obj[numI].parentElement.parentElement.scrollTop = numI*20;
+        } else {
+          obj[i].className = "";
+        }
+      }
+      this.printData(this.props.options[numI].id,this.props.options[numI].name);
+      this.setState({
+        indLi: numI,
+        txtOptionid: this.props.options[numI].id,
+        txtOptionname: this.props.options[numI].name
+      });
+    } else if(keyCode===13) {
+      /*===== reaction to pressing ENTER =====*/
+      if(this.state.tabIndex !== -1) {
+        //let myObj = document.getElementsByTagName("input");
+        this.objectOpenCloseFunc();
+      }
+    } else {
+      let arrCode = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 186, 188, 190, 219, 221, 222];
+      let numDop = 0;
+      for(let i=0; i<=arrCode.length;i++) {
+        if(arrCode[i] === keyCode) {
+          numDop = keyCode;
+        }
+      }
+      if(numDop !== 0) {
+        let keyText = this.state.keyText + e.key;
+        this.setState({
+          keyText: keyText
+        });
+        let myObjChild = document.getElementsByClassName("select");
+        for (let i = 0; i < this.props.options.length; i++) {
+          if( i === (+this.state.tabIndex) ) {
+            this.setState({
+              indLi: this.selectLineData(myObjChild[i]),
+              tabIndex: i
+            });
+          }
+        }
+      }
+    }
+  }
+/* checking the focus. */
+  inputFocusFunc(event) {
+    this.objectBlurFunc();
+    let evn = document.activeElement;
+    let tabIndex;
+
+    if (!evn || evn === document.body)
+    evn = null;
+    else if (document.querySelector)
+    evn = document.querySelector(":focus");
+    
+    let myObj = document.getElementsByTagName("input");
+    let widthEl;
+    for (let i = 0; i < myObj.length; i++) {
+      if( myObj[i] === evn ) {
+        myObj[i].className = "actionSelect";
+        tabIndex = i;
+        this.state.tabIndex = i;
+        this.state.widthObj = myObj[i].offsetWidth;
+        widthEl = myObj[i].offsetWidth;
+      } 
+    }
+    this.setState({
+      tabIndex: tabIndex,
+      widthObj: widthEl
+    });
+  }
+/* loss of focus. */
+  objectBlurFunc() {
+    let myObj = document.getElementsByTagName("input");
+    let myObjList = document.getElementsByClassName("listSelect");
+    for(let i = 0; i < myObj.length; i++) {
+      myObjList[i].className = "listSelect";
+      myObj[i].className = "";
+    }
+  }
+/* reaction to pressing ENTER. Opens and closes DropDown. */
+  objectOpenCloseFunc(){
+    let myObj = document.getElementsByTagName("input");
+    let myObjList = document.getElementsByClassName("listSelect");
+    if( myObjList[this.state.tabIndex].className === "listSelect")  {
+      myObjList[this.state.tabIndex].className += " active";
+      myObj[this.state.tabIndex].className = "activeInp";
+      this.bustList(myObj[this.state.tabIndex].value);
+      let obj = document.getElementsByTagName("li");
+      obj[this.state.indLi].className = "actionSelect";
+      obj[this.state.indLi].parentElement.parentElement.scrollTop = this.state.indLi*20;
+    } else {
+      myObjList[this.state.tabIndex].className = "listSelect";
+      myObj[this.state.tabIndex].className = "";
+    }
+  }
+  /* Bust list. Determine the selected item. */
+  bustList(item) {
+    let indLi; 
+    for(let i = 0; i < this.props.options.length; i++){
+      indLi = (this.props.options[i].name).includes(item) ? i : 0;
+    }
+    this.setState({
+      indLi: indLi
+    });
+  }
+  OptionSelectedFn(event, idLI, nameLI) {
+    this.printData(idLI,nameLI);
+    this.bustList(nameLI);
+    this.setState({
+      txtOptionid: idLI,
+      txtOptionname: nameLI
+    });
+    this.objectBlurFunc();
+  }
+  selectLineData(elem) {
+    let obj = elem.getElementsByTagName("li");
+    let numId = 0;
+    for(let i = 0; i < this.props.options.length; i++){
+      obj[i].className = "";
+    }
+    for(let i = 0; i < this.props.options.length; i++){
+      if(+numId === 0) {
+        if( (this.props.options[i].name).substr(0,this.state.keyText.length).indexOf(this.state.keyText) > -1 ){
+          numId = i;
+          obj[numId].className = "actionSelect";
+          elem.parentElement.scrollTop = numId*2;
+          this.setState({
+            txtOptionname: this.props.options[i].name,
+            txtOptionid: this.props.options[i].id,
+            indLi: i
+          });
+          break;
+        }
+      }
+    }
+    return numId;
+  }
+  /* printing it’s name and id to console */
+  printData(idSel,nameSel){
+    console.clear();
+    console.log("id:  "+idSel+"\nname:  "+nameSel);
+  }
   render() {
+    document.onkeydown = this.keyPressed;
     return (
-      <div>
-        <div 
-          className="textSelect" 
-          onClick={this.objectFocusFunc} 
-          onBlur={this.objectFocusFunc}
-        >
-          <i className={( this.props.flagOpen === false ) ? "fa fa-caret-down caret-down-icon" : "fa fa-caret-up caret-down-icon"}></i>
+      <div className="dropdownCls">
+        <div className="textSelect" >
           <input 
             type="text" 
-            value={this.props.firstoption} 
-            tabIndex={this.props.tabIndex} 
+            value={this.state.txtOptionname} 
+            onClick={this.objectOpenCloseFunc} 
+            onFocus={this.inputFocusFunc}
+            onBlur={this.objectBlurFunc} 
             readOnly 
           />
         </div>
-        <div className={( this.props.flagOpen === false ) ? "listSelect" : "listSelect active"}>
-          <div>
+        <div className="listSelect">
+         <div>
             <ul className="select">
-            {
-              this.props.options.map((option: DataProps, index: number) => 
+             {
+              this.props.options.map((option, index) => 
               <li key={option.id} className={(this.props.firstoption === option.name) ? "actionSelect" : ""}>
               <LISelect 
                 nameLI = {option.name}
                 idLI = {option.id}
-                widthObj = {this.props.widthObj}
+                widthObj = {this.state.widthObj}
                 functionOptionSelected = {this.OptionSelectedFn}
               />
               </li>
@@ -74,4 +252,5 @@ class UISelect extends Component<UIselectProps, UIselectState> {
     )
   }
 }
+
 export default UISelect
